8da162a6651ec31a6ee4dfb825ae6ffa222067b9
Enderson Maia <endersonmaia@gmail.com>
2026-09-08 10:39:25 -0300

    fix(alpine): inject KEY_NAME to setup-env.sh

--- alpine/setup-env.sh
+++ alpine/setup-env.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
 
-[ -f "/key/${KEY_NAME}.rsa" ] && \
-    cp /key/*.rsa.pub /etc/apk/keys/ && \
-    cp -a /key /root/.abuild && \
-    chown -R root:root /root/.abuild || true
-
 wget -q "${REMOTE_APK_REPO_URL}/keys/cartesi-apk-key.rsa.pub" \
     -O /etc/apk/keys/cartesi-apk-key.rsa.pub && \
     echo "${REMOTE_APK_REPO_URL}/stable" >> /etc/apk/repositories && \
     apk update -q || true
+
+[ -f "/key/${KEY_NAME}.rsa" ] && \
+    cp /key/*.rsa.pub /etc/apk/keys/ && \
+    cp -a /key /root/.abuild && \
+    echo "PACKAGER_PRIVKEY=/root/.abuild/${KEY_NAME}.rsa" > /root/.abuild/abuild.conf && \
+    chown -R root:root /root/.abuild || true
 
 find /root/packages/work -name 'APKINDEX.tar.gz' -size -1000c -delete 2>/dev/null || true
 
